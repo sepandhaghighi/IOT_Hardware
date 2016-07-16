8cbb406d15df1601b54e16da5a372a31902277cf
sepandhaghighi <sepand.haghighi@yahoo.com>
2016-07-17 01:02:35 +0430

    feat : first upload

---
+++ DHTtester.ino
@@ -0,0 +1,68 @@
+
+// Example testing sketch for various DHT humidity/temperature sensors
+// Written by ladyada, public domain
+#include "DigiKeyboard.h"
+#include "DHT.h"
+
+#define DHTPIN 2     // what digital pin we're connected to
+
+// Uncomment whatever type you're using!
+//#define DHTTYPE DHT11   // DHT 11
+#define DHTTYPE DHT22   // DHT 22  (AM2302), AM2321
+//#define DHTTYPE DHT21   // DHT 21 (AM2301)
+
+// Connect pin 1 (on the left) of the sensor to +5V
+// NOTE: If using a board with 3.3V logic like an Arduino Due connect pin 1
+// to 3.3V instead of 5V!
+// Connect pin 2 of the sensor to whatever your DHTPIN is
+// Connect pin 4 (on the right) of the sensor to GROUND
+// Connect a 10K resistor from pin 2 (data) to pin 1 (power) of the sensor
+
+// Initialize DHT sensor.
+// Note that older versions of this library took an optional third parameter to
+// tweak the timings for faster processors.  This parameter is no longer needed
+// as the current DHT reading algorithm adjusts itself to work on faster procs.
+DHT dht(DHTPIN, DHTTYPE);
+
+void setup() {
+    DigiKeyboard.println("DHL-Test!");
+
+  dht.begin();
+}
+
+void loop() {
+  // Wait a few seconds between measurements.
+  delay(2000);
+
+  // Reading temperature or humidity takes about 250 milliseconds!
+  // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
+  //float h = dht.readHumidity();
+  // Read temperature as Celsius (the default)
+  float t = dht.readTemperature();
+  // Read temperature as Fahrenheit (isFahrenheit = true)
+  //float f = dht.readTemperature(true);
+
+  // Check if any reads failed and exit early (to try again).
+  if (isnan(t)) {
+    DigiKeyboard.println("Failed to read from DHT sensor!");
+    return;
+  }
+
+  // Compute heat index in Fahrenheit (the default)
+  //float hif = dht.computeHeatIndex(f, h);
+  // Compute heat index in Celsius (isFahreheit = false)
+  //float hic = dht.computeHeatIndex(t, h, false);
+  //DigiKeyboard.print("Humidity: ");
+  //DigiKeyboard.print(h);
+  //DigiKeyboard.print(" %\t");
+  DigiKeyboard.print("Temperature: ");
+ DigiKeyboard.print(t);
+  DigiKeyboard.print(" *C ");
+  //DigiKeyboard.print(f);
+  //DigiKeyboard.print(" *F\t");
+  //DigiKeyboard.print(hic);
+  //DigiKeyboard.print(" *C ");
+  //DigiKeyboard.print(hif);
+  //DigiKeyboard.print(" *F");
+  
+}
