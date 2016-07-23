f5fbe6c237b9757587ad9377133128bcd0b4928f
Radin <radin.h@outlook.com>
2016-07-23 10:29:07 +0430

    Add files via upload

---
+++ MotherNode/MotherNode.ino
@@ -0,0 +1,16 @@
+
+String PostData = "{\"reqtype\":\"upload\",\"uid\":\"5\",\"password\":\"123\",\"temperature\":\"1000\",\"humidity\":\"1000\",\"pulse\":\"750\",\"reqtime\":\"\"}";
+
+void setup() {
+  // initialize the serial communication:
+  Serial.begin(115200);
+  // initialize the ledPin as an output:
+}
+
+void loop() {
+
+  delay(5000);
+  Serial.println(PostData);
+
+}
+
